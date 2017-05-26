a15de385f0df6ca6c395d388041f3fa923836a36
Marium Askri <maskri@sears.ca>
2017-05-26 18:43:01 +0000

    loadDocsToESCluster.js edited online with Bitbucket

--- sample-data/loadDocsToESCluster.js
+++ sample-data/loadDocsToESCluster.js
@@ -1,22 +1,3 @@
-
-/*var ElasticsearchCSV = require('elasticsearch-csv');
-
-// create an instance of the importer with options
-var esCSV = new ElasticsearchCSV({
-    es: { index: 'products', type: 'women', host: '127.0.0.1:9200' },
-    csv: { filePath: '<path-to-csv-file>', headers: true }
-});
-
-esCSV.import()
-    .then(function (response) {
-        // Elasticsearch response for the bulk insert
-        console.log(response);
-    }, function (err) {
-        // throw error
-        throw err;
-    });*/
-
-
 var MongoClient = require('mongodb').MongoClient;
 var elasticsearch = require('elasticsearch');
 var client = new elasticsearch.Client({
@@ -33,9 +14,9 @@ MongoClient.connect(url, function(err, db) {
         mongoElasticTransfer.transfer({
             esClient: client,
             mClient: db,
-            index: '<index_name>',
-            type: '<type>',
-            collection: '<mongoCollectionName>',
+            index: 'products',
+            type: 'women',
+            collection: 'women',
             preseveIds: true
         }, function (err) {
             if (!err)
